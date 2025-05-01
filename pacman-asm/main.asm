77dc8dcd32e243193087b45745af7e38dcb46261
ammonia2 <m500.abdullah@gmail.com>
2025-05-01 21:21:49 +0500

    Level1 Grid Initialisation

--- pacman-asm/main.asm
+++ pacman-asm/main.asm
@@ -3,19 +3,25 @@ INCLUDE Irvine32.inc
 .model flat, stdcall
 .stack 4096
 
-buffsize = 300 ; for file reading/writing
+buffsize = 2000 ; for file reading/writing
 
 ; Constants for menu navigation
-MENU_HOME      EQU 0
-MENU_LEVELS    EQU 1
-MENU_INSTR     EQU 2
-MENU_SCORES    EQU 3
-MENU_GAME      EQU 4
+MENU_HOME = 0
+MENU_LEVELS = 1
+MENU_INSTR = 2
+MENU_SCORES = 3
+MENU_GAME = 4
+
+GRID_WIDTH = 120
+GRID_HEIGHT = 30
+GRID_SIZE = GRID_WIDTH * GRID_HEIGHT
 
 .data
-    ; Global Variables
-    PACMAN         BYTE ?, ?
-    GRID           BYTE 60 DUP(?)
+    PACMAN         BYTE 'P'
+    wallChar       BYTE 219 ; Solid block character
+    dotChar        BYTE '.'
+    pacmanRow      DWORD 23
+    pacmanCol      DWORD 60
 
     ; HighScore File
     scoreFile      BYTE "highscore.txt", 0
@@ -23,19 +29,17 @@ MENU_GAME      EQU 4
     buffer         BYTE buffsize DUP(?)
 
     ; Player data
-    playerName     BYTE 31 DUP(0)    ; Player name (30 chars max + null)
+    playerName     BYTE 31 DUP(0)
     currentScore   DWORD 0
-    highScores     DWORD 5 DUP(0)    ; Store top 5 scores
-    highScoreNames BYTE 5 DUP(31 DUP(0)) ; Names for high scores
 
     ; Menu state
     currentMenu    DWORD MENU_HOME
     selectedLevel  DWORD 0
 
     ; Menu prompts and strings
-    titleArt       BYTE "=================================", 0Dh, 0Ah
-                   BYTE "           P A C M A N           ", 0Dh, 0Ah
-                   BYTE "=================================", 0Dh, 0Ah, 0
+    titleArt       BYTE "=================================================", 0Dh, 0Ah
+                   BYTE "                   P A C M A N                   ", 0Dh, 0Ah
+                   BYTE "=================================================", 0Dh, 0Ah, 0
 
     namePrompt     BYTE "Enter your name: ", 0
     levelPrompt    BYTE "Select level (1-5): ", 0
@@ -51,10 +55,10 @@ MENU_GAME      EQU 4
     keyPressMsg    BYTE "Press any key to return to menu...", 0
     returnMsg      BYTE "Press any key to return...", 0
     
-    
+
 .code
-INCLUDE utils.inc 
-INCLUDE menuUtils.inc 
+INCLUDE level1.inc
+INCLUDE menuUtils.inc
 
 main PROC
     call InitializeGame
@@ -68,25 +72,13 @@ InitializeGame PROC
     mov currentMenu, MENU_HOME
     mov currentScore, 0
     
-    ; Set default high scores for testing
-    mov highScores[0], 5000
-    mov highScores[4], 1000
-    mov highScores[8], 800
-    mov highScores[12], 500
-    mov highScores[16], 200
-    
-    ; Copy name strings (would use proper string copy in real implementation)
-    mov eax, OFFSET highScoreNames
-    mov BYTE PTR [eax], "P"
-    mov BYTE PTR [eax+1], "r"
-    mov BYTE PTR [eax+2], "o"
-    
-    mov eax, OFFSET highScoreNames+31
-    mov BYTE PTR [eax], "A"
-    mov BYTE PTR [eax+1], "c"
-    mov BYTE PTR [eax+2], "e"
-    
     ret
 InitializeGame ENDP
+
+startLevel1 PROC
+    call initialiseLevel1
+
+    ret
+startLevel1 ENDP
 
 END main
